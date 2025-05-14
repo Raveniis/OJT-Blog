ad6441503e10b2ef7d801feefbb506fc2258d750
Raveniis <128274869+Raveniis@users.noreply.github.com>
2025-05-14 15:56:53 +0800

    production config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideHttpClient(),
     provideRouter(routes),
-    provideAnimationsAsync(),
+    // provideAnimationsAsync(),
     { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
